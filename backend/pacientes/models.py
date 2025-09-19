c2c1e67fae4e84cbc3cc9bc62df2ea2fb63f9292
HerrThoMii <thomisz15@gmail.com>
2025-09-19 12:20:36 -0300

    Models

--- backend/pacientes/models.py
+++ backend/pacientes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import CASCADE
 
 # Create your models here.
+class Paciente(models.Model):
+    dni = models.IntegerField(primary_key=True, unique=True)
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    edad = models.IntegerField()
+    email = models.CharField(max_length=255)
+    numeroOS = models.IntegerField(unique=True)
+    idObraSocial = models.ForeignKey(ObraSocial, on_delete=CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.dni})"
+
+class EntradaAntecedente(models.Model):
+    fecha = models.DateField()
+    antecedente = models.TextField()
+
+    def __str__(self):
+        return f"{self.paciente} - {self.fecha}"
