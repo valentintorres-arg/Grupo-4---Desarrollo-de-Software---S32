6b2973089c88e5cc7c0e650e2d129ca17da833a1
valentintorres-arg <valentintorres4615@gmail.com>
2025-09-19 16:50:00 -0300

    Models update

--- backend/pacientes/models.py
+++ backend/pacientes/models.py
@@ -16,6 +16,7 @@ class Antecedente(models.Model):
     
     def __str__ (self):
         return f"{self.dni} {self.entradaAntecedenteID}"
+    
 class Paciente(models.Model):
     dni = models.IntegerField(primary_key=True, unique=True)
     nombre = models.CharField(max_length=255)
@@ -26,11 +27,38 @@ class Paciente(models.Model):
     idObraSocial = models.ForeignKey(ObraSocial, on_delete=CASCADE)
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido} ({self.dni})"
+        return f"{self.nombre} {self.apellido} {self.dni}"
 
 class EntradaAntecedente(models.Model):
+    entradaAntecedenteID = models.IntegerField(primary_key=True, unique=True)
     fecha = models.DateField()
     antecedente = models.TextField()
 
     def __str__(self):
-        return f"{self.paciente} - {self.fecha}"
+        return f"{self.paciente} {self.fecha} {self.antecedente} {self.entradaAntecedenteID}"
+class Pago(models.Model):
+    idPago = models.IntegerField(primary_key=True, unique=True)
+    total = models.FloatField(max_length=10)
+    fecha = models.DateField(auto_now_add=True)
+    pacienteId = models.ForeignKey(Paciente, on_delete=CASCADE)
+    consultaId = models.ForeignKey(Consulta, on_delete=CASCADE)
+    
+    def __str__(self):
+        return f"{self.idPago} {self.total} {self.fecha} {self.pacienteId} {self.consultaId}"
+    
+class Consulta(models.Model):
+    idConsulta = models.IntegerField(primary_key=True, unique=True)
+    descripcion = models.TextField()
+    fecha = models.DateField(auto_now_add=True)
+    
+    
+    
+    
+class Insumo(models.Model):
+    idInsumo = models.IntegerField(primary_key=True, unique=True)
+    nombre = models.CharField(max_lenght=100)
+    descripcion = models.TextField()
+    precioUnitario = models.FloatField(max_length=10)
+    
+    def __str__(self):
+        return f"{self.idInsumo} {self.nombre} {self.descripcion} {self.precioUnitario}"
