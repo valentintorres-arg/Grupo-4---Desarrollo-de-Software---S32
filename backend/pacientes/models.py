d15c8b53c3b28f98e308d828d479590cf970faa5
valentintorres-arg <valentintorres4615@gmail.com>
2025-09-19 12:24:18 -0300

    Models related to 'Pacientes'

--- backend/pacientes/models.py
+++ backend/pacientes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models import CASCADE
 
 # Create your models here.
+
+    
+    
+    
+    
+    
+    
+    
+class ObraSocial(models.Model):
+    idObraSocial = models.IntegerField(primary_key=True, unique=True)
+    nombre = models.CharField(max_lenght=55)
+    cobertura = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.cobertura} {self.idObraSocial}"
+    
+class Antecedente(models.Model):
+    dni = models.ForeignKey(Paciente, on_delete=CASCADE)
+    entradaAntecedenteID = models.ForeignKey(EntradaAntecedente, on_delete=CASCADE)
+    
+    def __str__ (self):
+        return f"{self.dni} {self.entradaAntecedenteID}"
